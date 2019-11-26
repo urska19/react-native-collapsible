7ed6f96ee329f885125aee347fb7e05c4d0b531c
Urska Pangerc <urska19@users.noreply.github.com>
2019-11-26 11:46:57 +0100

    add ViewPropTypes

--- Accordion.js
+++ Accordion.js
@@ -1,16 +1,17 @@
 import React, {
-  Component,
+  Component
 } from 'react';
 import PropTypes from 'prop-types';
 import {
   View,
   TouchableHighlight,
+  ViewPropTypes
 } from 'react-native';
 
 import Collapsible from './Collapsible';
 
 const COLLAPSIBLE_PROPS = Object.keys(Collapsible.propTypes);
-const VIEW_PROPS = Object.keys(View.propTypes);
+const VIEW_PROPS = Object.keys(ViewPropTypes);
 
 class Accordion extends Component {
   static propTypes = {
